397a8686a7e4eec2ba10f3dfbc77d935caa85e43
rgossington <rgossington@gmail.com>
2020-05-08 12:23:30 +0100

    initial version of setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='SettingsManager',
+      version='0.1.0dev',
+      description='Settings File Manager',
+      author='Rob Gossington',
+      author_email='rgossington@gmail.com',
+      url='https://github.com/rgossington/settings-manager',
+      packages=['settingsmanager'],
+      install_requires=[],
+      )
